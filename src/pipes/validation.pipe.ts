6690023dd1f419d77076c4b5a6dbc193f092c36d
katya <katya_yuneva@mail.ru>
2023-11-10 23:22:39 +0400

    add validation exeption and connect validation

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -1,8 +1,22 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import { ValidationException } from 'src/exceptions/validation.exception';
 
 @Injectable()
 export class ValidationPipe implements PipeTransform<any>{
-    transform(value: any, metadata: ArgumentMetadata): any{
+    async transform(value: any, metadata: ArgumentMetadata): Promise<any>{
+        const object = plainToClass(metadata.metatype, value);
+        const errors = await validate(object);
+
+        if(errors.length){
+            let messages = errors.map(
+                error => {
+                    return `${error.property} - ${Object.values(error.constraints).join(', ')}`
+                }
+            )
+            throw new ValidationException(messages);
+        }
         return undefined;
     }
 }
